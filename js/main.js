66e0f13b7713bc03c977d9156ae2d69b6f68282b
Fernando <ferelbue@gmail.com>
2024-01-29 22:39:39 +0100

    Se añade quienes somos en pantalla index

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 2000,
   touch: false
 })
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+
